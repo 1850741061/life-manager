d5fdb833f5bfc34a711b32ec42f1de26e6892e0f
stardustl1c <stardustl1c@users.noreply.gitee.com>
2026-01-05 19:49:28 +0800

    feat: add multi-account management with password save option

--- git_push.bat
+++ git_push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd H:\claude
 git add -A
-git commit -m "feat: completely optimize login interface and UX"
+git commit -m "feat: add multi-account management with password save option"
 git push origin main
 pause
